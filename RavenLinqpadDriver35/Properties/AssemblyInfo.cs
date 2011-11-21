7e32c08eb558c816d802316b19c3293a9e6bfbdc
Ronnie Overby <roverby@core-techs.net>
2011-11-21 12:22:09 -0500

    set version 0.2.0.0

--- RavenLinqpadDriver35/Properties/AssemblyInfo.cs
+++ RavenLinqpadDriver35/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("51a1106c-7851-43a3-9220-7a9ae4f8b612")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
